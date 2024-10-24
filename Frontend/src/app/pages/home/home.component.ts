384c202fbc7bd9fa318dba019aea04cfb1fdde0a
maria99r <maria99r@hotmail.com>
2024-10-24 21:08:00 +0200

    Pantalla de incio terminada

--- Frontend/src/app/pages/home/home.component.ts
+++ Frontend/src/app/pages/home/home.component.ts
@@ -3,12 +3,13 @@ import { NavComponent } from '../../components/nav/nav.component';
 import { CardModule } from 'primeng/card';
 import { FieldsetModule } from 'primeng/fieldset';
 import { ButtonModule } from 'primeng/button';
+import { FooterComponent } from "../../components/footer/footer.component";
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavComponent, CardModule, FieldsetModule, ButtonModule],
+  imports: [NavComponent, CardModule, FieldsetModule, ButtonModule, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
