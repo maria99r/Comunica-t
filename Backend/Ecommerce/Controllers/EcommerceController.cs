eb39f9b4772b141c9fcea193fee78dac90a534c6
Rocío <rgonfaus@gmail.com>
2024-10-24 21:12:42 +0200

    Creado controlador de ejemplo que devuelve todos los usuarios  con sus datos

---
+++ Backend/Ecommerce/Controllers/EcommerceController.cs
@@ -0,0 +1,23 @@
+﻿using Ecommerce.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EcommerceController : ControllerBase
+    {
+        private EcommerceContext _dbContext;
+
+        public EcommerceController(EcommerceContext dbContext) {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IEnumerable<User> GetUser()
+        {
+            return _dbContext.Users;
+
+        }
+    }
+}
