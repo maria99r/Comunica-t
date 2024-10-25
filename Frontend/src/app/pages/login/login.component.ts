de02588bb30d059d5267d97319ab689dee2b6cfa
David <dmargarb@g.educaand.es>
2024-10-25 18:12:35 +0200

    actualizar página de login

--- Frontend/src/app/pages/login/login.component.ts
+++ Frontend/src/app/pages/login/login.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ButtonModule],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  constructor(private http: HttpClient, private router: Router) {}
+
+  login() {
+    const urlServidor: string = '';
+    
+    
+    
+  }
 }
