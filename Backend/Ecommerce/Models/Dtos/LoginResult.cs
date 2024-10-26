80cb9629925a30315f55bb20c244988e9a5fa77f
maria99r <maria99r@hotmail.com>
2024-10-26 18:33:46 +0200

    Dtos user terminados

---
+++ Backend/Ecommerce/Models/Dtos/LoginResult.cs
@@ -0,0 +1,8 @@
+﻿namespace Ecommerce.Models.Dtos;
+
+public class LoginResult
+{
+    public string? AccessToken { get; set; }
+
+    public UserDto User { get; set; }   // Para que el front reciba datos del usuario que ha iniciado sesión
+}
