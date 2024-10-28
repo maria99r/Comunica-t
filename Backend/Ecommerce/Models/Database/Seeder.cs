90ed7ccfb074688896e62155a08787f5313dcfde
Maria Rosales <maria99r@hotmail.com>
2024-10-28 18:01:31 +0100

    comienzo productos en seeder

--- Backend/Ecommerce/Models/Database/Seeder.cs
+++ Backend/Ecommerce/Models/Database/Seeder.cs
@@ -16,6 +16,8 @@ public class Seeder
     public async Task SeedAsync()
     {
         await SeedUsersAsync();
+        await SeedProductAsync();
+
         await _context.SaveChangesAsync();
     }
 
@@ -41,4 +43,52 @@ public class Seeder
 
         await _context.Users.AddRangeAsync(users);
     }
+
+
+
+    private async Task SeedProductAsync()
+    {
+        Product[] products = [
+                new Product {
+                    Name = "Botones - Cuerpo Humano" ,
+                    Price = 10,
+                    Stock = 500,
+                    Description = "Tabla con botones con vocabulario del cuerpo humano.",
+                    Image = "products/botones-cuerpo.png"
+                }
+                ,
+                new Product {
+                    Name = "Tarjetas - Organizador tiempo" ,
+                    Price = 15,
+                    Stock = 350,
+                    Description = "Tarjetas para organizar las tareas diarias.",
+                    Image = "products/tarjetas-tiempo.png"
+                },
+                new Product {
+                    Name = "LOTE 20 Tarjetas - Organizador tiempo" ,
+                    Price = 350,
+                    Stock = 100,
+                    Description = "LOTE DE 20 - Tarjetas para organizar las tareas diarias.",
+                    Image = "products/tarjetas-tiempo.png"
+                },
+                new Product {
+                    Name = "LOTE 50 Tarjetas - Organizador tiempo" ,
+                    Price = 500,
+                    Stock = 100,
+                    Description = "Tarjetas para organizar las tareas diarias.",
+                    Image = "products/tarjetas-tiempo.png"
+                },
+                new Product {
+                    Name = "Tarjetas - Temática" ,
+                    Price = 12,
+                    Stock = 250,
+                    Description = "Tarjetas de tematica para niños.",
+                    Image = "products/tarjetas-tematica.png"
+                }
+            ];
+
+        await _context.Products.AddRangeAsync(products);
+    }
+
+
 }
