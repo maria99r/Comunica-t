16b1c922a453ab94b3f2b6161b8e0e3a7ee2d4bf
Dmargarb <145496969+Dmargarb@users.noreply.github.com>
2024-10-28 01:01:46 +0100

    Actualizar login

--- Frontend/src/app/pages/login/login.component.ts
+++ Frontend/src/app/pages/login/login.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { AuthRequest } from '../../models/auth-request';
 import { AuthService } from '../../services/auth.service';
+
 
 @Component({
   selector: 'app-login',
@@ -12,19 +13,23 @@ import { AuthService } from '../../services/auth.service';
   styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-  
+
   email: string = '';
   password: string = '';
   jwt: string = '';
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) { }
 
   async submit() {
-    const authData : AuthRequest = { email: this.email, password: this.password };
+    const authData: AuthRequest = { email: this.email, password: this.password };
     const result = await this.authService.login(authData);
 
     if (result.success) {
       this.jwt = result.data.accessToken;
+      alert('Has iniciado sesión correctamente.');
+      this.router.navigate(['/']);
+    } else {
+      alert('Datos de inicio de sesión incorrectos.');
     }
   }
 
