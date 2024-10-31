e7eee122c576b92ac1a19603fe14094362353db6
agu204 <aarigon742@g.educaand.es>
2024-10-31 21:03:57 +0100

    SobreNosotrosSinTerminar

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { CatalogComponent } from './pages/catalog/catalog.component';
+import { AboutUsComponent } from './pages/about-us/about-us.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },  // Ruta pantalla inicio home
     { path: 'login', component: LoginComponent }, // Ruta login
     { path: 'signup', component: SignupComponent}, // Ruta registro
-    { path: 'catalog', component: CatalogComponent} // Ruta catálogo
+    { path: 'catalog', component: CatalogComponent}, // Ruta catálogo
+    { path: 'about-us', component: AboutUsComponent} //Ruta sobre nosotros   
 ];
