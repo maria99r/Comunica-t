1babbfe3387dfe104a6618d6e247e0814ac8be3f
Maria Rosales <maria99r@hotmail.com>
2024-11-06 19:31:07 +0100

    vista detalle producto comienzo

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { CatalogComponent } from './pages/catalog/catalog.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },  // Ruta pantalla inicio home
     { path: 'login', component: LoginComponent }, // Ruta login
     { path: 'signup', component: SignupComponent}, // Ruta registro
-    { path: 'catalog', component: CatalogComponent} // Ruta catálogo
+    { path: 'catalog', component: CatalogComponent}, // Ruta catálogo
+    { path: 'product/:id', component: ProductDetailComponent }  // Ruta detalles de producto
 ];
