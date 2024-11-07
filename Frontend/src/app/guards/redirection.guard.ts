fecf78fd8d932a40b0c1a4b1c65413a9b86d58e6
Dmargarb <145496969+Dmargarb@users.noreply.github.com>
2024-11-07 20:06:49 +0100

    añadir redireccionamiento

---
+++ Frontend/src/app/guards/redirection.guard.ts
@@ -0,0 +1,22 @@
+import { ActivatedRouteSnapshot, CanActivateFn, Router, RouterStateSnapshot } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
+
+export const redirectionGuard: CanActivateFn = (
+  route: ActivatedRouteSnapshot, 
+  state: RouterStateSnapshot) => {
+
+    // Inyectamos servicios
+    const authService = inject(AuthService);
+    const router = inject(Router);
+
+    // Verificamos si el usuario está autenticado
+    if (!authService.isAuthenticated()) {
+      // Navegamos al login indicando que después redireccione a donde queríamos ir en un principio
+      router.navigate(['login'], { queryParams: { redirectTo: state.url }});
+      return false;
+    }
+
+    // Si está autenticado, permitimos la navegación
+  return true;
+};
