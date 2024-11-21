4d478fbeb2fbad5e207132d9b7fa9d6772167601
Dmargarb <145496969+Dmargarb@users.noreply.github.com>
2024-11-21 21:07:33 +0100

    checkout

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -7,13 +7,16 @@ import { CatalogComponent } from './pages/catalog/catalog.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
 import { CartComponent } from './pages/cart/cart.component';
+import { CheckoutComponent } from './pages/checkout/checkout.component';
+import { redirectionGuard } from './guards/redirection.guard';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },  // Ruta pantalla inicio home
     { path: 'login', component: LoginComponent }, // Ruta login
     { path: 'signup', component: SignupComponent}, // Ruta registro
     { path: 'catalog', component: CatalogComponent}, // Ruta catálogo
-    { path: 'about-us', component: AboutUsComponent}, //Ruta sobre nosotros   
+    { path: 'about-us', component: AboutUsComponent}, // Ruta sobre nosotros   
     { path: 'product/:id', component: ProductDetailComponent },  // Ruta detalles de producto
-    { path: 'cart', component: CartComponent}  //Ruta carrito
+    { path: 'cart', component: CartComponent},  // Ruta carrito
+    { path: 'checkout', component: CheckoutComponent, canActivate: [redirectionGuard] },  // Ruta checkout
 ];
--- Frontend/src/app/pages/checkout/checkout.component.ts
+++ Frontend/src/app/pages/checkout/checkout.component.ts
@@ -1,21 +1,104 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { StripeService } from 'ngx-stripe';
-import { StripeEmbeddedCheckout, StripeEmbeddedCheckoutOptions } from '@stripe/stripe-js';
+import {
+  StripeEmbeddedCheckout,
+  StripeEmbeddedCheckoutOptions,
+} from '@stripe/stripe-js';
+import { Product } from '../../models/product';
+import { Subscription } from 'rxjs';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { CheckoutService } from '../../services/checkout.service';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
   imports: [],
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.css'
+  styleUrl: './checkout.component.css',
 })
 
 //https://docs.stripe.com/checkout/embedded/quickstart
-export class CheckoutComponent implements OnInit{
-  
-  constructor(private stripeService: StripeService){}
+export class CheckoutComponent implements OnInit, OnDestroy {
+  @ViewChild('checkoutDialog')
+  checkoutDialogRef: ElementRef<HTMLDialogElement>;
 
-  ngOnInit(): void {
-      
+  product: Product = null;
+  sessionId: string = '';
+  routeQueryMap$: Subscription;
+  stripeEmbedCheckout: StripeEmbeddedCheckout;
+
+  constructor(
+    private service: CheckoutService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private stripe: StripeService
+  ) {}
+
+  ngOnInit() {
+    this.routeQueryMap$ = this.route.queryParamMap.subscribe((queryMap) =>
+      this.init(queryMap)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.routeQueryMap$.unsubscribe();
+  }
+
+  async init(queryMap: ParamMap) {
+    this.sessionId = queryMap.get('session_id');
+
+    if (this.sessionId) {
+      const request = await this.service.getStatus(this.sessionId);
+
+      if (request.success) {
+        console.log(request.data);
+      }
+    } else {
+      const request = await this.service.getAllProducts();
+
+      if (request.success) {
+        this.product = request.data[0];
+      }
+    }
+  }
+
+  async hostedCheckout() {
+    const request = await this.service.getHostedCheckout();
+
+    if (request.success) {
+      // Abrimos la url de la session de stripe sin crear una nueva pestaña en el navegador
+      window.open(request.data.sessionUrl, '_self');
+    }
+  }
+
+  async embeddedCheckout() {
+    const request = await this.service.getEmbededCheckout();
+
+    if (request.success) {
+      const options: StripeEmbeddedCheckoutOptions = {
+        clientSecret: request.data.clientSecret,
+      };
+
+      this.stripe.initEmbeddedCheckout(options).subscribe((checkout) => {
+        this.stripeEmbedCheckout = checkout;
+        checkout.mount('#checkout');
+        this.checkoutDialogRef.nativeElement.showModal();
+      });
+    }
+  }
+
+  reload() {
+    this.router.navigate(['checkout']);
+  }
+
+  cancelCheckoutDialog() {
+    this.stripeEmbedCheckout.destroy();
+    this.checkoutDialogRef.nativeElement.close();
   }
 }
