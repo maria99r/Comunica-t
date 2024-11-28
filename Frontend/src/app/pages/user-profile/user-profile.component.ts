83fcaf4be957042205f5d5c9bf5104bb6927db70
agu204 <aarigon742@g.educaand.es>
2024-11-28 20:03:45 +0100

    Update Perfl Usuario Se ve el nombre :)

--- Frontend/src/app/pages/user-profile/user-profile.component.ts
+++ Frontend/src/app/pages/user-profile/user-profile.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit  } from '@angular/core';
 import { FooterComponent } from "../../components/footer/footer.component";
 import { NavComponent } from "../../components/nav/nav.component";
+import { User } from '../../models/user';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-user-profile',
@@ -9,6 +11,20 @@ import { NavComponent } from "../../components/nav/nav.component";
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
-export class UserProfileComponent {
+export class UserProfileComponent  implements OnInit{
+  user: User | null = null; // Datos del usuario
+  isEditing = false; // Modo edición
+  orders: any[] = []; // Lista de pedidos
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.user = this.authService.getUser(); // Obtiene los datos del usuario autenticado
+    
+  }
+
+  toggleEdit(field: string) {
+    this.isEditing = !this.isEditing;
+    // Lógica adicional para habilitar la edición solo en el campo necesario
+  }
 }
