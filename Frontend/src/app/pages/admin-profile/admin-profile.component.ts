8acbb2767d4b614fb9404cb181550e97610e7713
agu204 <aarigon742@g.educaand.es>
2024-11-28 19:48:43 +0100

    Update Perfil

--- Frontend/src/app/pages/admin-profile/admin-profile.component.ts
+++ Frontend/src/app/pages/admin-profile/admin-profile.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../components/footer/footer.component";
+import { NavComponent } from "../../components/nav/nav.component";
 
 @Component({
   selector: 'app-admin-profile',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, NavComponent],
   templateUrl: './admin-profile.component.html',
   styleUrl: './admin-profile.component.css'
 })
--- Frontend/src/app/pages/user-profile/user-profile.component.ts
+++ Frontend/src/app/pages/user-profile/user-profile.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../components/footer/footer.component";
+import { NavComponent } from "../../components/nav/nav.component";
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, NavComponent],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
