81c09bd4fcf6c23f3ea72915355be096ed5d267e
agu204 <aarigon742@g.educaand.es>
2024-12-02 16:14:30 +0100

    Update Perfiles

--- Frontend/src/app/pages/admin-profile/admin-profile.component.ts
+++ Frontend/src/app/pages/admin-profile/admin-profile.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "../../components/footer/footer.component";
 import { NavComponent } from "../../components/nav/nav.component";
+import { User } from '../../models/user';
+import { AuthService } from '../../services/auth.service';
+import { Product } from '../../models/product';
 
 @Component({
   selector: 'app-admin-profile',
@@ -10,5 +13,21 @@ import { NavComponent } from "../../components/nav/nav.component";
   styleUrl: './admin-profile.component.css'
 })
 export class AdminProfileComponent {
+  user: User | null = null; //datos del usuario
+  isEditing = false; //modo edición
+  orders: any[] = []; //lista de pedidos
+  products: Product[] = []; // Lista de productos
+  
 
+  constructor(private authService: AuthService) {}
+
+  //obtiene los datos del usuario autenticado
+  ngOnInit() {
+    this.user = this.authService.getUser(); 
+  }
+
+  //logica para habilitar la edición solo en el campo necesario
+  toggleEdit(field: string) {
+    this.isEditing = !this.isEditing;
+  }
 }
