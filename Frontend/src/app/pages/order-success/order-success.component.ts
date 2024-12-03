c5eae9ec81c6c6dfeda7e0aaceb5696282a99863
Dmargarb <145496969+Dmargarb@users.noreply.github.com>
2024-12-03 13:25:51 +0100

    arreglo de errores y sustitución de alerts

--- Frontend/src/app/pages/order-success/order-success.component.ts
+++ Frontend/src/app/pages/order-success/order-success.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { NavComponent } from "../../components/nav/nav.component";
 import { FooterComponent } from "../../components/footer/footer.component";
 import { OrderService } from '../../services/order.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Order } from '../../models/order';
 import { CommonModule } from '@angular/common';
 import { ProductOrder } from '../../models/productOrder';
+import { AuthService } from '../../services/auth.service';
 
 
 @Component({
@@ -15,16 +16,23 @@ import { ProductOrder } from '../../models/productOrder';
   templateUrl: './order-success.component.html',
   styleUrl: './order-success.component.css'
 })
-export class OrderSuccessComponent {
+export class OrderSuccessComponent implements OnInit {
 
-  constructor(private orderService: OrderService, private activatedRoute: ActivatedRoute,
+  constructor(
+    private orderService: OrderService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService
   ) { }
 
-  id : number ;
-  order : Order = null;
-  products : ProductOrder[] = [];
+  id: number;
+  order: Order = null;
+  products: ProductOrder[] = [];
 
   async ngOnInit(): Promise<void> {
+    if (!this.authService.isAuthenticated()) {
+      this.router.navigate(['/']);
+    }
 
     const actualId = this.activatedRoute.snapshot.paramMap.get('id') as unknown as number;
 
