1f21240726184564f8b1e8c86b14da3bdf63d737
Maria Rosales <maria99r@hotmail.com>
2024-12-04 18:48:50 +0100

    vista admin muestra users y productos

--- Frontend/src/environments/environment.development.ts
+++ Frontend/src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     // en desarrollo
     apiUrl: "https://localhost:7185/api/",
-    apiImg: "https://localhost:7185/",
+     apiImg: "https://localhost:7185/",
 
     stripePublicKey: 'pk_test_51PV9x6RxpFCA5iRu06rDGzZpPVYlurYgDSP4vbpYDRgmnSJwEKymMEe0CPQtVBDSQ5zJmygTn6bEBh2zVmMNoT9s00CzqPY6dF'
 };
