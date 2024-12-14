a3ce7443a3aae5d892e9ec9baf4984157f7e92e4
urashio <116120484+rocigonf@users.noreply.github.com>
2024-12-14 16:45:00 +0100

    Formateadas descripciones
    Formateadas las descripciones de los productos para que en el front se muestren con saltos de líneas, etc.

--- Backend/Ecommerce/Models/Database/Seeder.cs
+++ Backend/Ecommerce/Models/Database/Seeder.cs
@@ -81,35 +81,35 @@ public class Seeder
                     Name = "Visualizador baño ocupado" ,
                     Price = 1999,
                     Stock = 24,
-                    Description = "Colocación: Colocar el dispositivo con el sensor de ultrasonido pegado en la pared encima de la cisterna del WC. Colocar el dispositivo con la pantalla TFT en la puerta del baño por la parte exterior.\r\nFuncionamiento: cuando algún usuario está usando el WC, la pantalla exterior se ilumina en rojo, indicando que el baño esta ocupado, en cualquier otro momento la pantalla exterior está iluminada en verde, indicando que se puede entrar al baño.\r\nAlimentación: conector USB de carga. Posibilidad de enchufarlo o powerbank.\r\nPosibilidad de adquirir una parte por separado (debe ser programado por personal especialista)\r\n",
+                    Description = "<b>Colocación:</b> Colocar el dispositivo con el sensor de ultrasonido pegado en la pared encima de la cisterna del WC. Colocar el dispositivo con la pantalla TFT en la puerta del baño por la parte exterior.<br><br><b>Funcionamiento:</b> Cuando algún usuario está usando el WC, la pantalla exterior se ilumina en rojo, indicando que el baño esta ocupado, en cualquier otro momento la pantalla exterior está iluminada en verde, indicando que se puede entrar al baño.<br><br><b>Alimentación:</b> Conector USB de carga.<br>- Posibilidad de enchufarlo o powerbank.<br>- Posibilidad de adquirir una parte por separado (debe ser programado por personal especialista)",
                     Image = "products/visualizadorbañoocupado.webp"
                 },
                 new Product {
                     Name = "Organizador de tareas" ,
                     Price = 2499,
                     Stock = 47,
-                    Description = "Dispositivo portátil de sobremesa. \r\nDisponible también en formato pared.\r\nFuncionamiento: el maestro y/o tutor dispone las tareas en orden en los soportes de tarjetas, para que el usuario las realice. El usuario cuando va a realizar la primera tarea la coge de su soporte y la pone en el dispositivo lector. El dispositivo emite un sonido \"leyendo la tarea\". En ese momento el usuario empieza a realizar la tarea. Cuando termina la misma, la coge y la introduce en el depósito de tareas. Así sucesivamente con las demás.\r\nAlimentación:4 pilas AA \r\nPosibilidad de ampliar tarjetas con pictogramas distintos bajo petición.\r\nPosibilidad de pedir soporte de tarjeta.\r\n",
+                    Description = "Dispositivo portátil de sobremesa.<br>Disponible también en formato pared.<br><br><b>Funcionamiento:</b> El maestro y/o tutor dispone las tareas en orden en los soportes de tarjetas, para que el usuario las realice. El usuario cuando va a realizar la primera tarea la coge de su soporte y la pone en el dispositivo lector. El dispositivo emite un sonido \"leyendo la tarea\". En ese momento el usuario empieza a realizar la tarea. Cuando termina la misma, la coge y la introduce en el depósito de tareas. Así sucesivamente con las demás.<br><br><b>Alimentación:</b> 4 pilas AA <br>- Posibilidad de ampliar tarjetas con pictogramas distintos bajo petición.<br>- Posibilidad de pedir soporte de tarjeta.",
                     Image = "products/organizador.webp"
                 },
                 new Product {
                     Name = "Pantalla TFT - Repuesto" ,
                     Price = 499,
                     Stock = 120,
-                    Description = "Pantalla TFT de repuesto para el visualizador de baño ocupado. Para su funcionamiento necesita dispositivo sensor ultrasonido.",
+                    Description = "Pantalla TFT de repuesto para el visualizador de baño ocupado.<br>Para su funcionamiento necesita dispositivo sensor ultrasonido.",
                     Image = "products/repuestos-visualizador/pantallatft.webp"
                 },
                 new Product {
                     Name = "Sensor ultrasonido - Repuesto" ,
                     Price = 699,
                     Stock = 256,
-                    Description = "Sensor ultrasonido de repuesto para el visualizador de baño ocupado. Para su funcionamiento necesita dispositivo pantalla TFT asociada.",
+                    Description = "Sensor ultrasonido de repuesto para el visualizador de baño ocupado.<br>Para su funcionamiento necesita dispositivo pantalla TFT asociada.",
                     Image = "products/repuestos-visualizador/sensorultrasonido.webp"
                 },
                 new Product {
                     Name = "Depósito tarjetas - Repuesto" ,
                     Price = 399,
                     Stock = 187,
-                    Description = "Depósito de tarjetas de repuesto para el organizador de tareas. Cuando el usuario termine la tarea, puede introducir la tarjeta en el depósito.",
+                    Description = "Depósito de tarjetas de repuesto para el organizador de tareas.<br>Cuando el usuario termine la tarea, puede introducir la tarjeta en el depósito.",
                     Image = "products/repuestos-organizador/deposito.webp"
                 },
                 new Product {
